5f74708ee8dd0708ab9bf0717c3cb45441e4048a
virginiakadel <vkadel@amphenol-antennas.com>
2022-08-22 06:16:03 -0400

    practices aug22

--- practices_one/prime_check.py
+++ practices_one/prime_check.py
@@ -1,22 +1,24 @@
 class Solution:
+
     def countPrimes(self, n: int) -> int:
-        #n=3
+
+        # n=3
         count = 0
-        if n in [0,1,2]:
+        if n in [0, 1, 2]:
             return 0
         else:
-            for i in range(3,n+1):
-                if n==1:
+            for i in range(3, n + 1):
+                if n == 1:
                     continue
-                is_print=True
-                print(f'i: {i // 2 + 1} Range to check: {list(range(2, i // 2 + 1))}')
-
-                for j in range(2, i // 2 + 1):
-                    if (i % j == 0):
-                        is_prime = False
-                        break
-                if is_print:
-                    count=count+1
+                if self.isPrime(self,i):
+                    count = count + 1
                 print(f'i:{i} c:{count}')
         print(f'count : {count}')
         return count
+
+    def isPrime(self, n):
+        for j in range(2, n // 2 + 1):
+            if (n % j == 0):
+                return False
+        return True
+
--- practices_one/test_prime_check.py
+++ practices_one/test_prime_check.py
@@ -3,5 +3,6 @@ import prime_check
 
 class TestSolution(TestCase):
     def test_count_primes(self):
-
+        assert prime_check.Solution.countPrimes(self,4)==2
+    def test_count_primes(self):
         assert prime_check.Solution.countPrimes(self,4)==2
